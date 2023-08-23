31b86d7f1fa546ae7376b4f0976382fe1ef69306
arib411 <soufian.arib@gmail.com>
2023-08-23 22:39:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	ssize_t read_line = 1;
+	size_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
