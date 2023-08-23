0562d8f78a51566871ae01e3eefa449236de6f55
arib411 <soufian.arib@gmail.com>
2023-08-23 23:08:08 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -7,7 +7,6 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -17,15 +16,12 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct stack_s
 {
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
-} 
-stack_t;
-
+} stack_t;
 /**
  * struct bus_s - variables -args, file, line content
  * @arg: value
@@ -34,17 +30,14 @@ stack_t;
  * @lifi: flag change stack <-> queue
  * Description: carries values through the program
  */
-
 typedef struct bus_s
 {
 	char *arg;
 	FILE *file;
 	char *content;
 	int lifi;
-}  
-bus_t;
+}  bus_t;
 extern bus_t bus;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -53,14 +46,11 @@ extern bus_t bus;
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
-} 
-instruction_t;
-
+} instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
@@ -85,5 +75,4 @@ void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
-
 #endif
